a37a54ee2e61bdc5e970d75df4ecb94285e73968
Arshad Ali Ansari <99089245+arshadaliansari786@users.noreply.github.com>
2025-04-15 16:23:48 +0530

    up

--- models/jobs.js
+++ models/jobs.js
@@ -4,7 +4,7 @@ const JobSchema = new mongoose.Schema({
     title: { type: String, required: true },
     description: { type: String, required: true },
     location: { type: String, required: true },
-    status: { type: String, enum: ['Open', 'Closed','Pause'], default: 'Open' },
+    status: { type: String, enum: ['Open', 'Closed', 'Pause'], default: 'Open' },
     candidates: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Candidate' }],
     postingDate: { type: Date, default: Date.now },
     flag: { type: Boolean, default: true },
